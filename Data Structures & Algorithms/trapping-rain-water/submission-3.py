0509241fa8bc925f1e75be371b8c448385db6557
Austin Delic <austin@austindelic.com>
2026-04-21 19:54:03 +0800

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        total = 0
+        for h in range(max(height), 0, -1):
+            indices = [i for i, x in enumerate(height) if x >= h]
+            if len(indices) < 2:
+                continue
+            for i in range(len(indices) - 1):
+                total += (indices[i + 1] - indices[i] -1)
+        return total
+        
