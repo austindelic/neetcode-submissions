c72ba14e2c96c602343d9db388ab3ee0467f1862
Austin Delic <austin@austindelic.com>
2026-04-21 15:24:06 +0800

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        p1 = 0
+        p2 = -1
+        s = "".join(c for c in s.lower() if c.isalnum())
+        for _ in range(len(s)):
+            if s[p1] != s[p2]:
+                return False
+            p1 += 1
+            p2 -= 1
+        return True
